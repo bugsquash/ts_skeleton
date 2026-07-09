a057e7b4071f7330d60993c127d8f333d2a447d9
Alejandro Napoles <14150661+lexnapoles@users.noreply.github.com>
2026-07-09 12:38:29 +0100

    Update build

---
+++ dist/src/position.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.position = position;
+function position(x, y) {
+    return {
+        x,
+        y
+    };
+}
---
+++ dist/test/initialiseRover.test.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("mocha");
+const assert_1 = require("assert");
+const node_test_1 = require("node:test");
+const position_1 = require("../src/position");
+const Rover_1 = require("../src/Rover");
+(0, node_test_1.describe)("Initialise Rover", () => {
+    (0, node_test_1.it)("Rover is initialised succesfully", () => {
+        const rover = new Rover_1.Rover((0, position_1.position)(2, 4));
+        assert_1.strict.deepStrictEqual(rover.position, { x: 2, y: 4 });
+    });
+});
