bfb1706d670827708029c506226c5e35c75bfe5c
Alejandro Napoles <14150661+lexnapoles@users.noreply.github.com>
2026-07-09 11:15:51 +0100

    Created main file

--- test/dummy.test.ts
+++
@@ -1,9 +0,0 @@
-import "mocha"
-import { strict as assert } from "assert";
-import {describe, it} from "node:test";
-
-describe("dummy", () => {
-    it("should pass", () => {
-        assert.equal(true, true);
-    });
-});
