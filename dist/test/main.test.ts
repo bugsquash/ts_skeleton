100bf9c176381e2a7bd9c2f41a4ce94638224012
Alejandro Napoles <14150661+lexnapoles@users.noreply.github.com>
2026-07-09 11:16:19 +0100

    Add main test

---
+++ dist/test/main.test.ts
@@ -0,0 +1,9 @@
+import "mocha"
+import { strict as assert } from "assert";
+import {describe, it} from "node:test";
+
+describe("main", () => {
+    it("work", () => {
+        assert.equal(true, true);
+    });
+});
