fcfacbe4b326c3ce1f6eae361021458844b99adc
Alejandro Napoles <14150661+lexnapoles@users.noreply.github.com>
2026-07-09 12:32:42 +0100

    Initialise rover with position

--- dist/test/initialiseRover.test.ts
+++ dist/test/initialiseRover.test.ts
@@ -4,15 +4,32 @@ import {describe, it} from "node:test";
 
 describe("Initialise Rover", () => {
   it("Rover is initialised succesfully", () => {
-    const rover = new Rover();
-    assert.deepStrictEqual(rover.position, {x: 2, y: 4})
+    const rover = new Rover(position(2, 4));
+    assert.deepStrictEqual(rover.position, {x: 2, y: 4});
   })
 })
 
 class Rover {
-  #position: number;
+  #position: Position;
+
+  constructor(position: Position) {
+    this.#position = position;
+  }
   
   get position() {
-    return {x: 2, y: 4}
+    return this.#position;
   }
 }
+
+type Position = {
+  x: number
+  y: number
+}
+
+function position(x: number, y: number) {
+  return {
+    x,
+    y
+  }
+}
+
